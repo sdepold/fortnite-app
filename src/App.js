3ac5338ec76c43723f260474021db73fb0b8eb8d
Sascha Depold <sdepold@ebay.com>
2020-03-09 13:48:26 +0100

    Remove styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import SearchForm from "./components/SearchForm";
 import WeaponsTable from "./components/WeaponsTable";
 
 import "./App.css";
@@ -8,7 +7,6 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <SearchForm />
       <WeaponsTable />
     </div>
   );
